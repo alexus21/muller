3e0c2c139e6a69f237d72252dc425bbf5a7c78a0
alexus21 <US21003@ues.edu.sv>
2023-06-20 17:27:35 -0600

    Agregando obtención de datos del formulario y sus verificaciones

--- mullerMethod/models.py
+++ mullerMethod/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class UserData(models.Model):
+    username = models.CharField(max_length=12)
+    password = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.user.username
--- mullerMethod/views.py
+++ mullerMethod/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import UserData
+
+# Validacion de datos:
+import re
 
 # Create your views here.
 
@@ -9,6 +13,43 @@ def login(request):
     # Lógica para el inicio de sesión
     return render(request, 'login.html')
 
+from django.shortcuts import render
+from .models import UserData
+
 def signup(request):
-    # Lógica para el registro de usuarios
+    if request.method =="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        retypedPassword = request.POST["retypePassword"]
+
+        print(checkUsername(username))
+
+        if password == retypedPassword:
+            datauser = UserData()
+            datauser.username = username
+            datauser.password = password
+            datauser.save()
+
     return render(request, 'signup.html')
+
+def checkUsername(username):
+    if checkIfUsernameIsEmpty(username):
+        return "El nombre de usuario no puede estar vacio"
+
+    if checkUsernameLen(username):
+        return "El nombre de usuario debe ser minimo de 5 letras"
+
+    if checkIfUsernameHasSymbols(username):
+        return "El nombre de usuario no puede contener símbolos"
+
+
+
+def checkIfUsernameIsEmpty(username):
+    return username == ""
+
+def checkUsernameLen(username):
+    return len(username) < 5
+
+def checkIfUsernameHasSymbols(username):
+    pattern = "^[a-zA-Z0-9]+$"
+    re.match(pattern, username)
