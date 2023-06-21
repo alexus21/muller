b23f9967ccaa8320f63a03ccb99cb5ad7bff471d
alexus21 <US21003@ues.edu.sv>
2023-06-20 19:55:05 -0600

    Reestructurando index.html

--- mullerMethod/static/js/snippets.js
+++ mullerMethod/static/js/snippets.js
@@ -1,7 +1,9 @@
-const exampleModal = document.getElementById('exampleModal');
+const signUpModal = document.getElementById('signupModal');
+const loginModal = document.getElementById('loginModal');
 
-if (exampleModal) {
-  exampleModal.addEventListener('show.bs.modal', event => {
+
+if (signUpModal) {
+  signUpModal.addEventListener('show.bs.modal', event => {
     // Button that triggered the modal
     const button = event.relatedTarget
     // Extract info from data-bs-* attributes
@@ -10,8 +12,26 @@ if (exampleModal) {
     // and then do the updating in a callback.
 
     // Update the modal's content.
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+    const modalTitle = signUpModal.querySelector('.modal-title')
+    const modalBodyInput = signUpModal.querySelector('.modal-body input')
+
+    // modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+
+if (loginModal) {
+  loginModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = loginModal.querySelector('.modal-title')
+    const modalBodyInput = loginModal.querySelector('.modal-body input')
 
     // modalTitle.textContent = `New message to ${recipient}`
     modalBodyInput.value = recipient
